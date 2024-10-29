c1edec7a63bef4c318f15a03b20ed33cc5b9f203
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-10-29 16:22:04 +0500

    CRUD Operations

--- index.js
+++ index.js
@@ -13,35 +13,37 @@ mongoose.connect('mongodb://127.0.0.1:27017/user').then(() => console.log("DataB
 const userSchema = new mongoose.Schema({
     first_name: {
         type: String,
-        required: true,
+
     },
     last_name: {
         type: String,
-        required: false,
+
     },
     email: {
         type: String,
-        required: true,
-        unique: true,
     },
     job_title: {
         type: String,
-        required: true,
+
     },
     address: {
         type: String,
-        required: true,
+
     }
-});
+},
+    {
+        timestamps: true,
+    });
 
 
 const usermodel = mongoose.model("user", userSchema);
 
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
 
+    const allUsersData = await usermodel.find({});
     //Creating html tag for rendering Data using Template String
-    const html = `<ul>${users.map((data) => `<li>${data.first_name}</li>`).join("")}</ul>`
+    const html = `<ul>${allUsersData.map((data) => `<li>${data.first_name} - ${data.email}</li> <br/>`).join("")}</ul>`
     return res.send(html)
 })
 
@@ -53,15 +55,19 @@ app.get('/users', (req, res) => {
 //     console.log("Calling Next Middle ware");
 //     next();
 // })
+
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
 
-app.get('/api/users', (req, res) => {
-    return res.json(users)
+app.get('/api/users', async (req, res) => {
+    const allUsersData = await usermodel.find({});
+
+    return res.json(allUsersData);
 });
 
 //Creating New User:
-app.post("/api/users", async(req, res) => {
+app.post("/api/users", async (req, res) => {
 
 
     const body = req.body;
@@ -69,101 +75,35 @@ app.post("/api/users", async(req, res) => {
     if (!body || !body.email || !body.first_name || !body.last_name || !body.email || !body.title || !body.gender) {
         return res.status(400).json({ msg: "All fields are required" })
     }
-    
-   await usermodel.create({
-        first_name:body.first_name,
-        last_name:body.last_name,
-        email:body.email,
-        job_title:body.title,
-        gender:body.gender,
+
+    const result = await usermodel.create({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        job_title: body.title,
+        gender: body.gender,
     });
 
     return res.status(201).send("User Successfully Created");
 })
 
 app.route('/api/users/:id')
-    .get((req, res) => {
-        const id = Number(req.params.id);
+    .get(async (req, res) => {
 
-        console.log("Id", id)
-        const user = users.find((user) => user && user.id === id);
+        const user = await usermodel.findById(req.params.id);
         if (!user) {
             return res.status(404).json({ msg: "Requested User not Exist" })
         }
 
         return res.json(user)
     })
-    .patch((req, res) => {
-
-        const email = req.body.email;
-        const id = req.params.id;
-        const findtoDobyId = (todos, id) => {
-
-            for (let i = 1; i < todos.length; i++) {
-                if (todos[i].id === parseInt(id)) {
-                    return i;
-                }
-            }
-
-        }
-
-        try {
-
-            fs.readFile('./MOCK_DATA.json', 'utf-8', (er, data) => {
-                if (er) {
-                    return res.status(404).send("Data not found");
-                }
-
-                // Parse and send the data
-                try {
-                    let todos = JSON.parse(data);
-                    const todoIndex = findtoDobyId(todos, id);
-
-                    todos[todoIndex].email = email;
-
-                    fs.writeFile('./MOCK_DATA.json', JSON.stringify(todos), () => {
-                        return res.json({ 'status': 'ok' })
-                    })
-                }
-                catch (parseErr) {
-                    res.status(500).send("Error parsing JSON data");
-                }
-            })
-        }
-        catch (err) {
-            console.log("Error", err)
-        }
-
-
-    })
-    .delete(("/api/users/:id", (req, res) => {
-
-
-        const id = Number(req.params.id);
-        fs.readFile('./MOCK_DATA.json', 'utf-8', (er, data) => {
-            if (er) {
-                return res.status(404).send("Data not found");
-            }
-
-            const findtoDobyId = (todos, id) => {
-
-                for (let i = 1; i < todos.length; i++) {
-                    if (todos[i].id === parseInt(id)) {
-                        return i;
-                    }
-                }
-
-            }
-
-            let jsonData = JSON.parse(data);
-            let index = findtoDobyId(jsonData, id);
-
-            let deletedData = jsonData.splice(index, 1);
-
-            fs.writeFile('./MOCK_DATA.json', JSON.stringify(deletedData), () => {
-                return res.json({ 'status': 'ok' })
-            })
+    .patch(async(req, res) => {
+        await usermodel.findByIdAndUpdate(req.params.id,{email:"none@mail.com"});
+        res.send({"msg":"Sucess"})
         })
+    .delete(("/api/users/:id",async (req, res) => {
+
+        await usermodel.findByIdAndDelete(req.params.id)
     }))
 
 
